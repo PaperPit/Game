a2b1211a544be0caf4a43ac679d9dbcc9c032d81
PaperPit <lustinmik@gmail.com>
2024-10-06 16:45:59 +0300

    Начало

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-print("Вот так надо")
+import pandas as pd
+from itertools import chain
+
+# Импортируем модуль pandas
+import pandas as pd 
+# Создаём объект DataFrame, загружая содержимое файла recipes.json
+df = pd.read_json('recipes.json') 
+
+# Выводим на экран первые строки полученного DataFrame
+combined_set = set(chain.from_iterable(df['ingredients']))
+print(len(combined_set))
+          
