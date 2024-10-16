5c006fbab541046573936d352a655275e28eb727
PaperPit <lustinmik@gmail.com>
2024-10-16 07:00:35 +0300

    HTML2.0

--- game.py
+++ game.py
@@ -1,7 +1,22 @@
-import requests # Импортируем библиотеку requests
-from bs4 import BeautifulSoup # Импортируем библиотеку BeautifulSoup
-url = 'https://nplus1.ru/news/2021/10/11/econobel2021' # Определяем адрес страницы
-response = requests.get(url) # Выполняем GET-запрос, содержимое ответа присваивается переменной response
-page = BeautifulSoup(response.text, 'html.parser') # Создаём объект BeautifulSoup, указывая html-парсер
-print(page.title) # Получаем тег title, отображающийся на вкладке браузера
-print(page.title.text) # Выводим текст из полученного тега, который содержится в атрибуте text
+import requests
+from bs4 import BeautifulSoup
+
+# Определяем адрес страницы
+url = 'https://nplus1.ru/news/2021/10/11/econobel2021'
+
+# Выполняем GET-запрос
+response = requests.get(url)
+
+# Создаём объект BeautifulSoup
+page = BeautifulSoup(response.text, 'html.parser')
+
+# Находим все meta теги с атрибутом name
+meta_description = page.find('meta', attrs={'itemprop': 'datePublished'})
+meta_og_title = page.find('meta', attrs={'property': 'og:site_name'})
+
+# Выводим значения атрибутов content для найденных meta-тегов
+if meta_description:
+    print("Meta Description:", meta_description['content'])
+
+if meta_og_title:
+    print("OG Title:", meta_og_title['content'])
