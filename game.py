884283a02bebd00ce1deace12c5b90f1d2567bdf
PaperPit <lustinmik@gmail.com>
2024-10-16 07:00:35 +0300

    HTML2.0

--- game.py
+++ game.py
@@ -1,11 +1,22 @@
-import pandas as pd
-from itertools import chain
+import requests
+from bs4 import BeautifulSoup
 
-# Импортируем модуль pandas
-import pandas as pd 
-# Создаём объект DataFrame, загружая содержимое файла recipes.json
-df = pd.read_json('recipes.json') 
+# Определяем адрес страницы
+url = 'https://nplus1.ru/news/2021/10/11/econobel2021'
 
-# Выводим на экран первые строки полученного DataFrame
-combined_set = set(chain.from_iterable(df['ingredients']))
-print(len(combined_set))
+# Выполняем GET-запрос
+response = requests.get(url)
+
+# Создаём объект BeautifulSoup
+page = BeautifulSoup(response.text, 'html.parser')
+
+# Находим все meta теги с атрибутом name
+meta_description = page.find('meta', attrs={'itemprop': 'datePublished'})
+meta_og_title = page.find('meta', attrs={'property': 'og:site_name'})
+
+# Выводим значения атрибутов content для найденных meta-тегов
+if meta_description:
+    print("Meta Description:", meta_description['content'])
+
+if meta_og_title:
+    print("OG Title:", meta_og_title['content'])
