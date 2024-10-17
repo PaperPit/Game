e5484a015b57b929ccca33870232b34451ddc864
PaperPit <lustinmik@gmail.com>
2024-10-17 06:31:37 +0300

    HTML функция парсер заголовков из википедии

--- game.py
+++ game.py
@@ -2,19 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 # Определяем адрес страницы
-url = 'https://www.zdorovo365.ru/vakansii.html'
+url = 'https://ru.wikipedia.org/wiki/Pink_Floyd'
+def wiki_header(url):
+    # Выполняем GET-запрос
+    response = requests.get(url)
+    # Создаём объект BeautifulSoup
+    page = BeautifulSoup(response.text, 'html.parser')
+    vacancy_header = page.find('span',class_='mw-page-title-main')
+    return vacancy_header.string
 
-# Выполняем GET-запрос
-response = requests.get(url)
 
-# Создаём объект BeautifulSoup
-page = BeautifulSoup(response.text, 'html.parser')
+print(wiki_header(url))
 
-vacancy_header = page.find('h3', string ="Наши вакансии")
-vacancy_position = page.find_all('span', class_='t')
-
-print(vacancy_header.string)
-# Выводим текст каждого элемента
-for i, position in enumerate(vacancy_position, 1):
-    print(f"Должность {i}: {position.text}")
 
